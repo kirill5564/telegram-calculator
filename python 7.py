066feb003da1e9ff27f486039cab8150a5c19a1f
kirill5564 <Krshemanaev1@mail.ru>
2024-11-01 02:05:29 +0300

    Add files via upload

---
+++ python 7.py
@@ -0,0 +1,39 @@
+import ptbot
+import os
+from telegram import message
+from pytimeparse import parse
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('TG_TOKEN')
+
+
+def reply(chat_id, text):
+    message_id = bot.send_message(chat_id, text)
+    bot.create_countdown(parse(text), notify, chat_id=chat_id, message_id=message_id, text=text)
+    bot.create_timer(parse(text), final_notify, chat_id=chat_id)
+
+
+def notify(secs_left, chat_id, message_id, text):
+    counter = parse(text) - secs_left
+    bot.update_message(chat_id, message_id, f"Осталось {secs_left} секунд!\n{render_progressbar(parse(text), counter)}")
+
+
+def final_notify(chat_id):
+    bot.send_message(chat_id, "Время вышло!")
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+if __name__ == '__main__':
+    bot = ptbot.Bot(TOKEN)
+    bot.reply_on_message(reply)
+    bot.run_bot()
